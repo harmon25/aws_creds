36f8d1b67545ea39f7acf170915053565442d34e
harmon25 <dougwright1@gmail.com>
2021-03-27 14:40:39 -0400

    fetching metadata

--- lib/aws_creds.ex
+++ lib/aws_creds.ex
@@ -1,5 +1,10 @@
 defmodule AwsCreds do
-  defstruct default_region: "", access_key_id: "", secret_access_key: "", session_token: nil
+  defstruct default_region: "",
+            access_key_id: "",
+            secret_access_key: "",
+            session_token: nil,
+            expiration: nil
+
   @locations [:app_env, :system_env, :config, :instance_role]
 
   @app :aws_creds
@@ -25,8 +30,6 @@ defmodule AwsCreds do
   end
 
   defp do_fetch(:app_env, opts) do
-    IO.inspect("checking app env")
-    IO.inspect(opts, label: "opts")
     profile = Keyword.fetch!(opts, :profile)
 
     Application.get_env(@app, :profiles, [])
@@ -40,9 +43,6 @@ defmodule AwsCreds do
   end
 
   defp do_fetch(:system_env, opts) do
-    IO.inspect("checking system env")
-    IO.inspect(opts, label: "opts")
-
     {System.get_env(@aws_access_key_id), System.get_env(@aws_secret_access_key)}
     |> case do
       {nil, _} ->
@@ -64,28 +64,39 @@ defmodule AwsCreds do
   end
 
   defp do_fetch(:config, opts) do
-    IO.inspect("parsing config file")
-    IO.inspect(opts, label: "opts")
     home = System.user_home!()
     creds_file = ".aws/credentials"
+    creds_path = Path.join([home, creds_file])
 
-    {:ok, parse_result} =
-      Path.join([home, creds_file])
-      |> ConfigParser.parse_file()
+    File.read(creds_path)
+    |> case do
+      {:error, :enoent} ->
+        :cont
 
-    profile_str = Atom.to_string(opts[:profile])
+      {:ok, config_contents} ->
+        {:ok, parse_result} = ConfigParser.parse_string(config_contents)
 
-    if ConfigParser.has_section?(parse_result, profile_str) do
-      {:halt, {opts[:profile], new(parse_result[profile_str])}}
-    else
-      :cont
+        profile_str = Atom.to_string(opts[:profile])
+
+        if ConfigParser.has_section?(parse_result, profile_str) do
+          {:halt, {opts[:profile], new(parse_result[profile_str])}}
+        else
+          :cont
+        end
     end
   end
 
   defp do_fetch(:instance_role, opts) do
-    IO.inspect(opts, label: "opts")
-    IO.inspect("fetching instance role creds")
-    :cont
+    runtime_type()
+    |> fetch_instance_creds()
+    |> case do
+      {:ok,
+       %{"AccessKeyId" => access_key_id, "SecretAccessKey" => secret_access_key, "Token" => token}} ->
+        {:halt, {opts[:profile], new(access_key_id, secret_access_key, region(), token)}}
+
+      _ ->
+        :cont
+    end
   end
 
   defp region() do
@@ -118,18 +129,94 @@ defmodule AwsCreds do
   end
 
   def runtime_type() do
+    fetch_imdsv2_token()
+    |> case do
+      {:ok, token} ->
+        {:ec2, token}
+
+      _ ->
+        {:ecs, nil}
+    end
+  end
+
+  def fetch_iam_info(token) do
+    "http://169.254.169.254/latest/meta-data/iam/info"
+    |> http_get_req([{"x-aws-ec2-metadata-token", token}])
+    |> case do
+      {:ok, json_str} ->
+        Jason.decode(json_str)
+
+      _ ->
+        {:error, :not_ec2}
+    end
+  end
+
+  def parse_role_arn("arn:aws:iam::" <> rest) do
+    String.split(rest, "/")
+    |> List.last()
+  end
+
+  defp fetch_role(token) do
+    fetch_iam_info(token)
+    |> case do
+      {:ok, %{"InstanceProfileArn" => arn}} -> parse_role_arn(arn)
+      {:error, :not_ec2} -> {:error, :not_ec2}
+    end
+  end
+
+  defp fetch_instance_creds({:ec2, token}) do
+    role = fetch_role(token)
+
+    "http://169.254.169.254/latest/meta-data/iam/security-credentials/#{role}"
+    |> http_get_req([{"x-aws-ec2-metadata-token", token}])
+    |> case do
+      {:ok, json_str} ->
+        Jason.decode(json_str)
+
+      _ ->
+        {:error, :not_ec2}
+    end
+  end
+
+  defp fetch_instance_creds({:ecs, _}) do
+    relative_uri = System.get_env("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
+
+    http_get_req("http://169.254.170.2" <> relative_uri)
+    |> case do
+      {:ok, json_str} ->
+        Jason.decode(json_str)
+
+      _ ->
+        {:error, :not_ec2}
+    end
+  end
+
+  def http_get_req(url, headers \\ []) do
+    :hackney.request(
+      :get,
+      url,
+      headers
+    )
+    |> case do
+      {:ok, 200, _headers, ref} ->
+        :hackney.body(ref)
+
+      _ ->
+        {:error, :not_ec2}
+    end
+  end
+
+  def fetch_imdsv2_token() do
     :hackney.request(
       :put,
       "http://169.254.169.254/latest/api/token",
-      [headers: [{"X-aws-ec2-metadata-token-ttl-seconds", 21600}]],
+      [{"x-aws-ec2-metadata-token-ttl-seconds", "21600"}],
       "",
       connect_timeout: 1000
     )
-  end
-
-  defp fetch_meta(:ec2) do
-  end
-
-  defp fetch_meta(:ecs) do
+    |> case do
+      {:ok, 200, _headers, ref} -> :hackney.body(ref)
+      _ -> {:error, :none}
+    end
   end
 end
